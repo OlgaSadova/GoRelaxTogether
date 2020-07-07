baa128e93d7c98a861e78586f1c563118122551b
Olga Sadova <sadovaolga1@gmail.com>
2020-07-07 16:13:10 -0700

    Dialogs Page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,28 @@ import  classes from './Dialogs.module.css';
 
 export default function Dialogs() {
     return (
-        <div>
-           Dialogs 
+        <div className={classes.dialogs}>
+         <div className={classes.dialogsItem}>
+             <div className={classes.dialog + ' ' + classes.active}>
+                 Olga
+             </div>
+             <div className={classes.dialog}>
+                 Sergiy
+             </div>
+             <div className={classes.dialog}>
+                 Mira
+             </div>
+             <div className={classes.dialog}>
+                 Milana
+             </div>
+        </div>
+         <div className={classes.message}>
+             <div className={classes.messages}>
+                 <div className={classes.message}> Hello!!!</div>
+                 <div className={classes.message}> What do you like to do?</div>
+                 <div className={classes.message}>I love hiking!</div>
+             </div>
+         </div>
         </div>
     )
 }
