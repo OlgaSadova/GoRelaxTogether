4a50b53452c321413803a8e83c30985f53d59abe
Olga Sadova <sadovaolga1@gmail.com>
2020-09-29 22:28:16 -0700

    add more

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -12,30 +12,36 @@ const MessageItem = (props)=>{
    
 }
 export default function Dialogs() {
-    let dialogsData = [{id: 1, name: 'Olga'}, 
+    let dialogs = [{id: 1, name: 'Olga'}, 
                        {id: 2, name: 'Sergiy'},
                        {id: 3, name: 'Mira'},
                        {id: 4, name: 'Milana'},
                        {id: 5, name: 'Lina'}
                     ]
-    let messageData = [{id: 1, message: 'Hi'}, 
-                       {id: 2, message: 'What do you like to do?'},
-                       {id: 3, message: 'I love hiking!'},
-                       {id: 4, message: 'Hi, Me too'},
-                       {id: 5, message: 'I hate sport'}
-                    ]
+    let messages = [{id: 1, message: 'Hi'}, 
+                    {id: 2, message: 'What do you like to do?'},
+                    {id: 3, message: 'I love hiking!'},
+                    {id: 4, message: 'Hi, Me too'},
+                    {id: 5, message: 'I hate sport'}]
+
+    let dialogsElements = dialogs.map(dialog =>  <DialogItem name={dialog.name} id={dialog.id}/>);
+   
+   
+   
+                    
+    let messageElements = messages.map(message =>  <MessageItem message={message.message} id={message.id}/>);  
    
     return (
         <div className={classes.dialogs}>
          <div className={classes.dialogsItem}>
-             <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-             <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+            
+            {dialogsElements}
             
         </div>
          <div className={classes.message}>
              <div className={classes.messages}>
-                <MessageItem message={messageData[0].message} id={messageData[0].id}/>
-                <MessageItem message={messageData[1].message} id={messageData[1].id}/>
+               
+                {messageElements}
                 
              </div>
          </div>
--- src/components/Profile/Myposts/Posts.jsx
+++ src/components/Profile/Myposts/Posts.jsx
@@ -3,11 +3,14 @@ import classes from '../Myposts/Posts.module.css';
 import Post from './Post/Post.jsx'; 
 export default function Posts() {
 
-    let postData = [
+    let posts = [
     {id: 1, message: 'What do you like to do?', likesCount: 10, dontLikeCount: 0}, 
-    {id: 2, message: 'I like swimming!What about you?', likesCount: 80, dontLikeCount: 1},
-   
+    {id: 2, message: 'I like swimming!What about you?', likesCount: 70, dontLikeCount: 1},
+    {id: 3, message: 'I hate swimming!What about you?', likesCount: 0, dontLikeCount: 1},
+    {id: 4, message: 'I like swimming!What about you?', likesCount: 70, dontLikeCount: 1},
  ]
+
+ let postsElements = posts.map(post => <Post message={post.message} likesCount={post.likesCount} dontLikeCount={post.dontLikeCount}/>)
 
     return (
         <div>
@@ -17,9 +20,8 @@ export default function Posts() {
            <div><textarea></textarea></div>
            <button>Add Post</button>
            <button>Delete Post</button>
-       <Post message={postData[0].message} likesCount={postData[0].likesCount} dontLikeCount={postData[0].dontLikeCount}/>
        
-       <Post message={postData[1].message} likesCount={postData[1].likesCount} dontLikeCount={postData[1].dontLikeCount}/>
+       {postsElements}
        <Post/> 
        </div>
      </div>
