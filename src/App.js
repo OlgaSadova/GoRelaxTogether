630b33f771afeabf6e1b40e9364eff215e333115
Olga Sadova <sadovaolga1@gmail.com>
2020-10-07 14:59:31 -0700

    Replace

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import News from './components/News/News.jsx';
 import Music from './components/Music/Music.jsx';
 import Settings from './components/Settings/Settings.jsx';
-function App() {
+
+function App(props) {
   return (
     <BrowserRouter>
     <div className='app-wrapper'>
@@ -16,11 +17,11 @@ function App() {
     <Navbar/>
     <div className='app-wrapper-content'> 
    
-    <Route path='/profile' component={Profile}/>
-    <Route path='/dialogs' component={Dialogs}/>
-    <Route path='/news' component={News}/>
-    <Route path='/music' component={Music}/>
-    <Route path='/settings' component={Settings}/>
+    <Route path='/profile' render={()=><Profile/>}/>
+    <Route path='/dialogs' render={()=><Dialogs/>}/>
+    <Route path='/news' render={()=><News/>}/>
+    <Route path='/music' render={()=><Music/>}/>
+    <Route path='/settings' render={()=><Settings/>}/>
     </div>
      </div>
      </BrowserRouter>
